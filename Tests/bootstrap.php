30d991603cbb82c1c8c42b6b7ecfa0c758a7b584
Robert Gruendler <robert@dubture.com>
2015-03-10 22:09:43 +0100

    added webhook support

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -3,3 +3,5 @@
 if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
     throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
 }
+
+require_once __DIR__ . '/../vendor/autoload.php';
